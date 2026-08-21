7f68d60f6fc97310177baba00989964b9e467c0b
Suprim Bhandari <suprim.b@yahoo.com>
2026-08-21 07:42:47 -0400

    Update: Refine design documentation and enhance FileService with pre-check for directory existence

--- Models/FileItemTypes.cs
+++ Models/FileItemTypes.cs
@@ -1,5 +1,10 @@
+using System.Text.Json.Serialization;
+
 namespace TestProject.Models;
 
+
+//Tells the ASP.NET's JSON serializer to use the enum's name instead of using number.
+[JsonConverter(typeof(JsonStringEnumConverter))]
 //The enum used by FileItems to classify file and directory in a controlled manner.
 public enum FileItemType
 {
